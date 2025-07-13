e96b2db9e3bc5c4f85cae176185bcfeac89b872c
Sulochana Peiris <sulosp1992@gmail.com>
2025-07-13 12:14:24 +0200

    .java

---
+++ .java/Example.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+        
+		System.out.print("Average marks : ");
+		double avg=input.nextDouble();
+		System.out.println("Average : "+avg);
+	} 
+}
